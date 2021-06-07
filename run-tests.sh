eb11e707e6d8e803daa6fc26bc41958470b04ea8
Nicola <ntarocco@gmail.com>
2021-06-07 17:51:15 +0200

    citations generation: update style files (#22)
    * citations generation: update submodule style files
    * fix tests on GitHub actions
    * closes inveniosoftware/invenio-rdm-records#626
    
    Co-authored-by: ines-cruz <inesruud7@gmail.com>

--- run-tests.sh
+++ run-tests.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015-2020 CERN.
+# Copyright (C) 2015-2021 CERN.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -14,6 +14,4 @@ set -o errexit
 set -o nounset
 
 python -m check_manifest --ignore ".*-requirements.txt"
-python -m sphinx.cmd.build -qnNW docs docs/_build/html
 python -m pytest
-python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
