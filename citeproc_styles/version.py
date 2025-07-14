7ee262bfc082d3b75e8110f5d118096145a6ddc5
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-07-14 22:53:01 +0200

    release: v0.1.4

--- citeproc_styles/version.py
+++ citeproc_styles/version.py
@@ -2,6 +2,7 @@
 #
 # This file is part of citeproc-py-styles.
 # Copyright (C) 2016-2018 CERN.
+# Copyright (C) 2025 Graz University of Technology.
 #
 # citeproc-py-styles is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -14,4 +15,4 @@ and parsed by ``setup.py``.
 
 from __future__ import absolute_import, print_function
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
